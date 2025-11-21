9bd3be4daddfda1431132d12fec7ef6dce576b02
Marlon Brendon Eger Pedrosp <75400607+MarlonBEP@users.noreply.github.com>
2025-11-21 11:37:22 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,25 +1,17 @@
 import os
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes
-from telegram.ext import filters
+from telegram.ext import Application, CommandHandler
 
-TOKEN = os.environ.get("BOT_TOKEN")
+TOKEN = os.getenv("BOT_TOKEN")
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Bot de tarefas ativado! Me envie atividades e eu organizo!")
+async def start(update, context):
+    await update.message.reply_text("Bot online no Railway!")
 
-async def registrar(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    texto = update.message.text
-    await update.message.reply_text("Anotado! 👍")
-
-async def main():
-    app = ApplicationBuilder().token(TOKEN).build()
+def main():
+    app = Application.builder().token(TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, registrar))
 
-    await app.run_polling()
+    app.run_polling()
 
 if __name__ == "__main__":
-    import asyncio
-    asyncio.run(main())
+    main()
